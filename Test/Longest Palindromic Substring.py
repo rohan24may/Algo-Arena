388c4a3541a739f25d57b83e13a8b09bc33597b2
rohan24may <rohanabcd0305@gmail.com>
2026-04-07 18:45:53 +0530

    New Test Questions

---
+++ Test/Longest Palindromic Substring.py
@@ -0,0 +1,23 @@
+#2️⃣ Longest Palindromic Substring 🔥 👉 Return longest palindrome in string
+
+def longest_pal(s):
+    res = ""
+
+    for i in range(len(s)):
+        # odd
+        l = r = i
+        while l>=0 and r<len(s) and s[l]==s[r]:
+            if (r-l+1) > len(res):
+                res = s[l:r+1]
+            l -= 1
+            r += 1
+
+        # even
+        l, r = i, i+1
+        while l>=0 and r<len(s) and s[l]==s[r]:
+            if (r-l+1) > len(res):
+                res = s[l:r+1]
+            l -= 1
+            r += 1
+
+    return res
