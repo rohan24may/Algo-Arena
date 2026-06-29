d6b7d50595093c2026f838f2c7787d3866207d53
rohan24may <rohanabcd0305@gmail.com>
2026-06-29 21:03:23 +0530

    Add implementations for 2Sum and 3Sum problems

---
+++ 2 Pointer/2SumAgain.py
@@ -0,0 +1,39 @@
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+# find two numbers such that they add up to a specific target number.
+
+# Return the indices of the two numbers (1-indexed).
+
+# You may not use the same element twice.
+
+# There is exactly one solution.
+
+# --------------------------------------------------
+
+# Example 1:
+
+# Input:
+# numbers = [2,7,11,15]
+# target = 9
+
+# Output:
+# [1,2]
+
+numbers = [2, 7, 11, 15]
+target = 9
+
+left = 0
+right = len(numbers) - 1
+
+while left < right:
+
+    current_sum = numbers[left] + numbers[right]
+
+    if current_sum == target:
+        print([left + 1, right + 1])
+        break
+
+    elif current_sum > target:
+        right -= 1
+
+    else:
+        left += 1
---
+++ 2 Pointer/3sum.py
@@ -0,0 +1,66 @@
+"""15. 3Sum (Medium)
+Question:
+Given an integer array nums, return all the triplets
+[nums[i], nums[j], nums[k]]
+such that:
+
+i != j
+i != k
+j != k
+
+nums[i] + nums[j] + nums[k] == 0
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Input:
+nums = [-1,0,1,2,-1,-4]
+
+Output:
+[[-1,-1,2],[-1,0,1]]
+"""
+
+nums = [-1,0,1,2,-1,-4]
+
+nums.sort()
+
+answer = []
+
+for i in range(len(nums)):
+
+    # Skip duplicate fixed numbers
+    if i > 0 and nums[i] == nums[i-1]:
+        continue
+
+    left = i + 1
+    right = len(nums) - 1
+
+    target = -nums[i]
+
+    while left < right:
+
+        current_sum = nums[left] + nums[right]
+
+        if current_sum == target:
+
+            answer.append([nums[i], nums[left], nums[right]])
+
+            left += 1
+            right -= 1
+
+            # Skip duplicate left values
+            while left < right and nums[left] == nums[left - 1]:
+                left += 1
+
+            # Skip duplicate right values
+            while left < right and nums[right] == nums[right + 1]:
+                right -= 1
+
+        elif current_sum < target:
+            left += 1
+
+        else:
+            right -= 1
+
+print(answer)
