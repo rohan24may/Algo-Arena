013b992ebeed54a3ba766c3eb9e9d6a7a4c6cce3
rohan24may <rohanabcd0305@gmail.com>
2026-07-13 18:13:06 +0530

    Add implementation for Move Zeroes algorithm

---
+++ 2 Pointer/Array/MoveZeros2.py
@@ -0,0 +1,29 @@
+"""
+283. Move Zeroes
+
+Given an integer array nums, move all 0's to the end of it while maintaining
+the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+"""
+
+nums = [0, 1, 0, 3, 12]
+
+
+def moveZeroes(nums):
+    j = 0
+
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[i], nums[j] = nums[j], nums[i]
+            j += 1
+
+    return nums
+
+
+print(moveZeroes(nums))
