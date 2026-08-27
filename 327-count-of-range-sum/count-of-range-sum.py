294dc54349efaa2bc6b5a9f3b5da4b79f8c7f69c
Rohan Hudati <rohanabcd0305@gmail.com>
2026-08-28 00:40:08 +0530

    Time: 1263 ms (53.20%) | Memory: 52.6 MB (25.10%) - LeetSync

--- 327-count-of-range-sum/count-of-range-sum.py
+++ 327-count-of-range-sum/count-of-range-sum.py
@@ -1,28 +1,21 @@
 class Solution:
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
 
-        # Build prefix sum
         prefix = [0]
-
         for num in nums:
             prefix.append(prefix[-1] + num)
 
-        # Merge Sort function
         def merge_sort(left, right):
             if right - left <= 1:
                 return 0
 
             mid = (left + right) // 2
 
-            count = merge_sort(left, mid)
-            count += merge_sort(mid, right)
+            count = merge_sort(left, mid) + merge_sort(mid, right)
 
-            # Count valid pairs
-            k = mid
-            j = mid
+            k = j = mid
 
             for i in range(left, mid):
-
                 while k < right and prefix[k] - prefix[i] < lower:
                     k += 1
 
@@ -31,23 +24,7 @@ class Solution:
 
                 count += j - k
 
-            # Merge the two sorted halves
-            merged = []
-            p1 = left
-            p2 = mid
-
-            while p1 < mid and p2 < right:
-                if prefix[p1] <= prefix[p2]:
-                    merged.append(prefix[p1])
-                    p1 += 1
-                else:
-                    merged.append(prefix[p2])
-                    p2 += 1
-
-            merged.extend(prefix[p1:mid])
-            merged.extend(prefix[p2:right])
-
-            prefix[left:right] = merged
+            prefix[left:right] = sorted(prefix[left:right])
 
             return count
 
