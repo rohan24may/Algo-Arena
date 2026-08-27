b5a90114fc975c98b66beacabc8fe7ed88e01dc2
Rohan Hudati <rohanabcd0305@gmail.com>
2026-08-27 15:05:10 +0530

    Time: 11 ms (72.12%) | Memory: 20.7 MB (23.41%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.py
+++ 1242-matrix-block-sum/matrix-block-sum.py
@@ -4,16 +4,38 @@ class Solution:
         rows = len(mat)
         cols = len(mat[0])
 
+        # 1. Create prefix matrix
+        prefix = [[0] * (cols + 1) for _ in range(rows + 1)]
+
+        # 2. Build 2D prefix sum
+        for i in range(rows):
+            for j in range(cols):
+                prefix[i + 1][j + 1] = (
+                    prefix[i][j + 1]
+                    + prefix[i + 1][j]
+                    - prefix[i][j]
+                    + mat[i][j]
+                )
+
+        # 3. Create answer matrix
         answer = [[0] * cols for _ in range(rows)]
 
+        # 4. Calculate each block sum
         for i in range(rows):
             for j in range(cols):
 
-                block_sum = 0
+                top = max(0, i - k)
+                bottom = min(rows - 1, i + k)
 
-                for r in range(max(0, i - k), min(rows, i + k + 1)):
-                    for c in range(max(0, j - k), min(cols, j + k + 1)):
-                        block_sum += mat[r][c]
+                left = max(0, j - k)
+                right = min(cols - 1, j + k)
+
+                block_sum = (
+                    prefix[bottom + 1][right + 1]
+                    - prefix[top][right + 1]
+                    - prefix[bottom + 1][left]
+                    + prefix[top][left]
+                )
 
                 answer[i][j] = block_sum
 
