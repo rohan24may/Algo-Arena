ccd32cff31e0eb42a86227e1faea5c9e749b8f29
Rohan Hudati <rohanabcd0305@gmail.com>
2026-08-29 12:48:43 +0530

    Time: 1 ms (42.75%) | Memory: 21.3 MB (89.39%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -5,21 +5,20 @@ class Solution:
 
         for interval in intervals:
 
-            # 1. Before
+            # Before
             if interval[1] < newInterval[0]:
                 ans.append(interval)
 
-            # 2. Overlap
-            elif interval[0] <= newInterval[1]:
+            # After
+            elif interval[0] > newInterval[1]:
+                ans.append(newInterval)
+                newInterval = interval
+
+            # Overlap
+            else:
                 newInterval[0] = min(newInterval[0], interval[0])
                 newInterval[1] = max(newInterval[1], interval[1])
 
-            # 3. After
-            else:
-                ans.append(interval)
-
         ans.append(newInterval)
-
-        ans.sort(key=lambda x: x[0])
 
         return ans
